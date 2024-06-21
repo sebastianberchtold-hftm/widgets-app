aa1368bcbcd227fb4c921f62768dd21289f5d5d1
sebastianberchtold-hftm <sebastian.berchtold@hftm.ch>
2024-06-21 17:31:02 +0200

    blog api mit appwrite.io backend

--- ui_controls_demo/lib/blog.dart
+++ ui_controls_demo/lib/blog.dart
@@ -6,7 +6,7 @@ class Blog {
   bool isLikedByMe = false;
 
   Blog({
-    this.id = 0,
+    required this.id,
     required this.title,
     required this.content,
     required this.publishedAt,
@@ -14,4 +14,22 @@ class Blog {
 
   String get publishedDateString =>
       "${publishedAt.day}.${publishedAt.month}.${publishedAt.year}";
+
+  factory Blog.fromJson(Map<String, dynamic> json) {
+    return Blog(
+      id: json['\$id'],
+      title: json['title'],
+      content: json['content'],
+      publishedAt: DateTime.parse(json['publishedAt']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'content': content,
+      'publishedAt': publishedAt.toIso8601String(),
+      'isLikedByMe': isLikedByMe,
+    };
+  }
 }
--- ui_controls_demo/lib/blog_repository.dart
+++ ui_controls_demo/lib/blog_repository.dart
@@ -1,79 +1,27 @@
 import 'dart:async';
 
 import 'blog.dart';
+import 'blog_api.dart';
 
 class BlogRepository {
-  // Static instance + private Constructor for simple Singleton-approach
   static BlogRepository instance = BlogRepository._privateConstructor();
   BlogRepository._privateConstructor();
 
-  final _blogs = <Blog>[];
+  final BlogApi _api = BlogApi();
 
-  int _nextId = 1;
-
-  bool _isInitialized = false;
-
-  void _initializeBlogs() async {
-    addBlogPost(Blog(
-      title: "Flutter ist toll!",
-      content:
-          "Mit Flutter hebst du deine App-Entwicklung auf ein neues Level. Probier es aus!",
-      publishedAt: DateTime.now(),
-    ));
-
-    addBlogPost(Blog(
-      title: "Der Kurs ist dabei abzuheben",
-      content:
-          "Fasten your seatbelts, we are ready for takeoff! Jetzt geht's ans Eingemachte. Bleib dabei!",
-      publishedAt: DateTime.now().subtract(const Duration(days: 1)),
-    ));
-
-    addBlogPost(Blog(
-      title: "Klasse erzeugt eine super App",
-      content:
-          "Während dem aktiven Plenum hat die Klasse alles rausgeholt und eine tolle App gebaut. Alle waren begeistert dabei und haben viel gelernt.",
-      publishedAt: DateTime.now().subtract(const Duration(days: 2)),
-    ));
-
-    _isInitialized = true;
-  }
-
-  /// Returns all blog posts ordered by publishedAt descending.
-  /// Simulates network delay.
   Future<List<Blog>> getBlogPosts() async {
-    if (!_isInitialized) {
-      _initializeBlogs();
-    }
-
-    await Future.delayed(const Duration(milliseconds: 500));
-
-    return _blogs..sort((a, b) => b.publishedAt.compareTo(a.publishedAt));
+    return await _api.fetchBlogPosts();
   }
 
-  /// Creates a new blog post and sets a new id.
   Future<void> addBlogPost(Blog blog) async {
-    blog.id = _nextId++;
-    _blogs.add(blog);
+    await _api.createBlogPost(blog);
   }
 
-  /// Deletes a blog post.
-  Future<void> deleteBlogPost(Blog blog) async {
-    _blogs.remove(blog);
+  Future<void> deleteBlogPost(int id) async {
+    await _api.deleteBlogPost(id);
   }
 
-  /// Changes the like info of a blog post.
-  Future<void> toggleLikeInfo(int blogId) async {
-    final blog = _blogs.firstWhere((blog) => blog.id == blogId);
-    blog.isLikedByMe = !blog.isLikedByMe;
-  }
-
-  /// Updates a blog post with the given id.
-  Future<void> updateBlogPost(
-      {required int blogId,
-      required String title,
-      required String content}) async {
-    final blog = _blogs.firstWhere((blog) => blog.id == blogId);
-    blog.title = title;
-    blog.content = content;
+  Future<void> updateBlogPost(Blog blog) async {
+    await _api.updateBlogPost(blog);
   }
 }
