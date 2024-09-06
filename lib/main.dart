bd08dbdc1a0c928e8f82e1135080065b3547ea8c
sebastianberchtold-hftm <sebastian.berchtold@hftm.ch>
2024-09-06 10:59:37 +0200

    Slidable widget added

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'features/blog/presentation/pages/add_blog_page.dart';
+import 'features/blog/presentation/pages/blog_list_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -23,116 +22,6 @@ class BlogApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: BlogListPage(),
-    );
-  }
-}
-
-class BlogListPage extends StatefulWidget {
-  @override
-  _BlogListPageState createState() => _BlogListPageState();
-}
-
-class _BlogListPageState extends State<BlogListPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Blog List'),
-      ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('blogs').snapshots(),
-        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
-            return Center(child: Text('No blogs available.'));
-          }
-
-          List<QueryDocumentSnapshot> blogs = snapshot.data!.docs;
-
-          return ListView.builder(
-            itemCount: blogs.length,
-            itemBuilder: (context, index) {
-              var blogData = blogs[index].data() as Map<String, dynamic>;
-              return ListTile(
-                title: Text(blogData['title'] ?? 'No Title'),
-                subtitle: Text(blogData['content'] ?? 'No Content'),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BlogDetailPage(blogData: blogData),
-                    ),
-                  );
-                },
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            PageRouteBuilder(
-              pageBuilder: (context, animation, secondaryAnimation) =>
-                  AddBlogPage(),
-              transitionsBuilder:
-                  (context, animation, secondaryAnimation, child) {
-                const begin =
-                    Offset(0.0, 1.0); // Start position (bottom of screen)
-                const end = Offset.zero; // End position (top of screen)
-                const curve = Curves.ease;
-
-                var tween = Tween(begin: begin, end: end)
-                    .chain(CurveTween(curve: curve));
-                var offsetAnimation = animation.drive(tween);
-
-                return SlideTransition(
-                  position: offsetAnimation,
-                  child: child,
-                );
-              },
-            ),
-          );
-        },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-class BlogDetailPage extends StatelessWidget {
-  final Map<String, dynamic> blogData;
-
-  const BlogDetailPage({Key? key, required this.blogData}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(blogData['title'] ?? 'No Title'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              blogData['title'] ?? 'No Title',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 8),
-            Text(
-                'Published on: ${blogData['publishedDateString'] ?? 'Unknown Date'}'),
-            SizedBox(height: 16),
-            Text(blogData['content'] ?? 'No Content',
-                style: TextStyle(fontSize: 16)),
-          ],
-        ),
-      ),
     );
   }
 }
